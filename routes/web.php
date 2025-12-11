e1a72d14c0fc09875bfb2ed7261dda0fa68f3bc6
MaouNour <nourg0870@gmail.com>
2025-12-12 02:29:56 +0300

    fixing some missancoulses in the Dashboard

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,16 @@ Route::post('/login',[SessionController::class,'login']);
 //TODO middleWare
 // Route::view('/Dashboard','User.index');
 Route::get('/Dashboard',function(){
-    $users = User::simplePaginate(10);
+    $users = $users = User::where('is_admin',false)->orderByRaw("CASE WHEN user_verified_at IS NULL THEN 0 ELSE 1 END, user_verified_at ASC")
+        ->simplePaginate(10);
+    $verified_users = User::where('is_admin',false)->whereNotNull('user_verified_at');
+    $unverified_users = User::where('is_admin',false)->whereNull('user_verified_at');
+    $admins = User::where('is_admin',true);
     return view('User.index', [
-        'users'=>$users
+        'users'=>$users,
+        'verified_users'=>$verified_users,
+        'unverified_users'=>$unverified_users,
+        'admins'=>$admins
     ]);
 });
+
